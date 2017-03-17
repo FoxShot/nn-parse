f1a9f78b79f44bc75e8c8632cd5e0589a1cc75ab
FoxShot <vulpiculae@gmail.com>
2017-03-17 19:45:54 +0200

    Gladen tuoksuinen gui

--- gtk_nn_parse.py
+++ gtk_nn_parse.py
@@ -1,59 +1,11 @@
- #!python3
+#!/usr/bin/env python3
 
-from lxml import html
-import requests
-import re
-import math
-import urllib
-import vlc
-from os.path import expanduser
-home = expanduser("~")	#multi os support
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk,Gdk,GObject
 from gi.repository.GdkPixbuf import Pixbuf
-import os
-import configparser
-#import mpv
-#from mpv import MPV
-import youtube_dl
-
-class login(requests.Session):
-	def __init__(self):
-		requests.Session.__init__(self)
-		config = configparser.SafeConfigParser()
-		configfile = open(home + '/.config/nn-parse/config','r')
-		config.readfp(configfile)
-		username = config.get("user", "username")
-		password = config.get("user", "password")
-		payload = {
-			"username": username,
-			"password": password
-		}
-		login_url = "http://naurunappula.com/login.php?ref=%2F"
-		result = self.post(
-			login_url,
-			data = payload,
-			headers = dict(referer=login_url)
-		)
-		tree = html.fromstring(result.content)		
-		self.user_id = tree.xpath('//a[@class="userphoto"]/@href')[0]
-		result = self.get("https://naurunappula.com"+self.user_id)
-		tree = html.fromstring(result.content)
-		self.group_ids = list()
-		self.group_names = list()
-		for kanava in tree.xpath('//div[@class="grouplist"]/ul/li'):
-			link = kanava.xpath('a/@href')[0]
-			self.group_ids.append(re.search('\d+', link).group(0))
-			self.group_names.append(kanava.xpath('a/text()')[0])
-			
-	def hae(self, url):
-		return self.get(
-			url,
-			headers = dict(referer = url)
-		)
-
-mie = login()
+import vlc
+from nn_parse import *
 
 class VLCWidget(Gtk.DrawingArea):
 	"""Simple VLC widget.
@@ -89,20 +41,15 @@ class Kanavavalikko(Gtk.ComboBoxText):
 class KommenttiLaatikko(Gtk.ListBoxRow):
 	def __init__(self, kommentti):
 		Gtk.ListBoxRow.__init__(self)
-		vbox = Gtk.VBox()
-		hbox = Gtk.HBox()
-		user_vbox = Gtk.VBox()
-		user_vbox.pack_start(Gtk.Label("<"+kommentti.user+">"), False, False, 0)
-		user_vbox.pack_start(Gtk.Label(kommentti.user_data), False, False, 0)
-		hbox.pack_start(user_vbox, False, False, 10)
-		content = Gtk.TextView()
-		content.set_wrap_mode(Gtk.WrapMode.WORD)
-#		content.set_justification(Gtk.GTK_JUSTIFY_LEFT)
-		bufferri = content.get_buffer()
-		bufferri.set_text(kommentti.text)
-		hbox.pack_end(content, True, True, 0)
-		vbox.add(hbox)
-		self.add(vbox)
+		builder = Gtk.Builder()
+		builder.add_from_file("KommenttiLaatikko.glade")
+		user_name = builder.get_object("user_name")
+		user_name.set_label("<"+kommentti.user+">")
+		user_data = builder.get_object("user_data")
+		user_data.set_label(kommentti.user_data)
+		comment = builder.get_object("comment")
+		comment.get_buffer().set_text(kommentti.text)
+		self.add(builder.get_object("comment_box"))
 
 class DataBox(Gtk.VBox):
 	def __init__(self, olio):	
@@ -259,27 +206,6 @@ class VLCWindow(Gtk.Window):
 		self.vbox.pack_end(self.tiedot, False, False, 0)
 		self.queue_draw()
 		self.show_all()
-		
-class YTelement(youtube_dl.YoutubeDL):
-	def __init__(self, url):
-		options = {
-			'format': '-f bestvideo[height<=480]+bestaudio/best[height<=480]',
-			'simulate': 'true'
-			}
-		youtube_dl.YoutubeDL.__init__(self, options)
-		with self:
-			result = self.extract_info(
-				'http:' + url,
-				download=False # We just want to extract the info
-			)
-		if 'entries' in result:
-			# Can be a playlist or a list of videos
-			formats = result['entries'][0]['formats']
-		else:
-			# Just a video
-			formats = result['formats']
-		
-		self.video = result['url']
 
 class Thumbnail(Gtk.Image):
 	def __init__(self, url):
@@ -299,103 +225,6 @@ class Thumbnail(Gtk.Image):
 
 		pb = Pixbuf.new_from_file("./icons/"+imgname)
 		self.set_from_pixbuf(pb)
-
-class NNement(list):
-	def __init__(self, page=1):
-		self.url = 'http://naurunappula.com/videot'
-		result = mie.hae(self.url+'/?p='+str(page))
-#		result = requests.get(self.url)
-		tree = html.fromstring(result.content)
-		for element in tree.xpath('//table[@class="padd gridlist"]/tr/td/a'):
-			self.append(VideoElement(element))
-			
-class Kommentti:
-	def __init__(self, element):
-		self.user = element.xpath('td[@class="author"]//b/text()')[0]
-		self.user_data = " ".join(element.xpath('td[@class="author"]/div[@class="usergroup"]//text()')).strip()
-		self.content = element.xpath('td[@class="content"]/div/text()')
-		self.text = " ".join(self.content)
-
-class VideoElement:
-	def __init__(self, gridlist):
-		self.title = gridlist.xpath('@title')[0]
-		if self.title == "":
-			self.name = "<Ei nimikettä>"
-		else:
-			self.name = gridlist.xpath('text()')[0]
-		self.link = "https://naurunappula.com" + gridlist.xpath('@href')[0]
-#		self.link_id = re.search('\d+', self.link).group(0)
-		self.image = gridlist.xpath('img/@src')[0]
-	
-	def hae_video(self):
-		result = mie.get(self.link)
-		tree = html.fromstring(result.content)
-		linkdata = tree.xpath('//div[@id="view_container"]/div[@id="linkdatacontainer"]/div[@id="linkdata"]')[0]
-
-		self.link_id = tree.xpath('//input[@name="link_id"]/@value')[0]
-		self.rating = linkdata.xpath('h1/span[@id="ratevalue"]/text()')
-		if len(self.rating)!=0:
-			self.rating = self.rating[0].strip()
-		else:
-			self.rating = "<et ole kirjautunut>"
-		self.katsottu = linkdata.xpath('div[@id="linktoolstable"]//p/b/text()')[0]
-		self.user = linkdata.xpath('div[@id="linktoolstable"]//a/b/text()')
-		if len(self.user)!=0:
-			self.user = self.user[0]
-			i=2
-		else:
-			self.user = "anon"
-			i=1 #jos anon niin yksi rivi puuttuu alusta
-		self.date = linkdata.xpath('div[@id="linktoolstable"]//div[@id="linkinfo"]/p/text()')[i].strip()
-		self.date = re.search('\d+[.]\d+[.]\d+', self.date).group(0) #haetaan vain päivänmäärä
-
-		embedded = tree.xpath('//div[@id="viewbody_container"]/div[@id="viewbody"]/div[@id="viewembedded"]')[0]
-		youtube = embedded.xpath('iframe/@src')
-		flv = embedded.xpath('script/text()')
-
-		if len(flv) != 0:
-			self.url = re.search('https:.+[.]flv', flv[0]).group(0) #HUOM! jos logattuna niin https, muuten http
-		else:
-			ydl = YTelement(youtube[0])
-			self.url = ydl.video
-	
-	def hae_sessio(self, sessio):
-		tree = html.fromstring(sessio.content)
-		self.title = tree.xpath('//div[@id="view_container"]/div[@id="linkdatacontainer"]/div[@id="linkdata"]/h1/span[@id="linktitle"]/text()')
-		if len(self.title)!=0:
-			self.title = self.title[0]
-		else:
-			self.title = "<Ei nimikettä>"
-		self.link = sessio.url
-		self.hae_video()
-	
-	def hae_rating(self):
-		result = mie.hae(self.link)
-		tree = html.fromstring(result.content)
-		linkdata = tree.xpath('//div[@id="view_container"]/div[@id="linkdatacontainer"]/div[@id="linkdata"]')[0]
-		self.rating = linkdata.xpath('h1/span[@id="ratevalue"]/text()')[0].strip()
-		
-	def hae_kommentit(self):
-		result = mie.get(self.link)
-		tree = html.fromstring(result.content)
-		self.comments = list()
-		for kommentti in tree.xpath('//div[@id="list_comments"]/table/tr')[::2]:
-			self.comments.append(Kommentti(kommentti))
-		
-	def rate_video(self, rating):
-		payload={
-			'service_id':'1',
-			'target_id':self.link_id,
-			'points':rating
-			}
-		mie.post("https://naurunappula.com/rate.php", payload)
-		
-	def add_channel(self, kanava):
-		payload={
-			'link_id':self.link_id,
-			'group_id':kanava
-			}
-		mie.post("https://naurunappula.com/favadd.php", payload)
 
 class Nappi(Gtk.Button):
 	def __init__(self, olio):
@@ -449,8 +278,6 @@ class MyWindow(Gtk.Window):
 		self.grid = Ristikko()
 		self.jako.pack_start(self.grid, True, True, 0)
 	
-#		self.player = mpv.MPV(ytdl=True)
-
 	def on_button_clicked(self, widget):
 		self.page += widget.Mnemonic
 		self.jako.remove(self.grid)
@@ -458,7 +285,6 @@ class MyWindow(Gtk.Window):
 		self.jako.pack_start(self.grid, True, True, 0)
 		self.queue_draw()
 		self.show_all()		#tämä oli tärkeä
-	
 
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
---
+++ nn_parse.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+from lxml import html
+import requests
+import re
+import math
+import urllib
+from os.path import expanduser
+home = expanduser("~")	#multi os support
+import os
+import configparser
+import youtube_dl
+
+class login(requests.Session):
+	def __init__(self):
+		requests.Session.__init__(self)
+		config = configparser.SafeConfigParser()
+		configfile = open(home + '/.config/nn-parse/config','r')
+		config.readfp(configfile)
+		username = config.get("user", "username")
+		password = config.get("user", "password")
+		payload = {
+			"username": username,
+			"password": password
+		}
+		login_url = "http://naurunappula.com/login.php?ref=%2F"
+		result = self.post(
+			login_url,
+			data = payload,
+			headers = dict(referer=login_url)
+		)
+		tree = html.fromstring(result.content)		
+		self.user_id = tree.xpath('//a[@class="userphoto"]/@href')[0]
+		result = self.get("https://naurunappula.com"+self.user_id)
+		tree = html.fromstring(result.content)
+		self.group_ids = list()
+		self.group_names = list()
+		for kanava in tree.xpath('//div[@class="grouplist"]/ul/li'):
+			link = kanava.xpath('a/@href')[0]
+			self.group_ids.append(re.search('\d+', link).group(0))
+			self.group_names.append(kanava.xpath('a/text()')[0])
+			
+	def hae(self, url):
+		return self.get(
+			url,
+			headers = dict(referer = url)
+		)
+
+mie = login()
+		
+class YTelement(youtube_dl.YoutubeDL):
+	def __init__(self, url):
+		options = {
+			'format': '-f bestvideo[height<=480]+bestaudio/best[height<=480]',
+			'simulate': 'true'
+			}
+		youtube_dl.YoutubeDL.__init__(self, options)
+		with self:
+			result = self.extract_info(
+				'http:' + url,
+				download=False # We just want to extract the info
+			)
+		if 'entries' in result:
+			# Can be a playlist or a list of videos
+			formats = result['entries'][0]['formats']
+		else:
+			# Just a video
+			formats = result['formats']
+		
+		self.video = result['url']
+
+class NNement(list):
+	def __init__(self, page=1):
+		self.url = 'http://naurunappula.com/videot'
+		result = mie.hae(self.url+'/?p='+str(page))
+#		result = requests.get(self.url)
+		tree = html.fromstring(result.content)
+		for element in tree.xpath('//table[@class="padd gridlist"]/tr/td/a'):
+			self.append(VideoElement(element))
+			
+class Kommentti:
+	def __init__(self, element):
+		self.user = element.xpath('td[@class="author"]//b/text()')[0]
+		self.user_data = " ".join(element.xpath('td[@class="author"]/div[@class="usergroup"]//text()')).strip()
+		self.content = element.xpath('td[@class="content"]/div/text()')
+		self.text = " ".join(self.content)
+
+class VideoElement:
+	def __init__(self, gridlist):
+		self.title = gridlist.xpath('@title')[0]
+		if self.title == "":
+			self.name = "<Ei nimikettä>"
+		else:
+			self.name = gridlist.xpath('text()')[0]
+		self.link = "https://naurunappula.com" + gridlist.xpath('@href')[0]
+#		self.link_id = re.search('\d+', self.link).group(0)
+		self.image = gridlist.xpath('img/@src')[0]
+	
+	def hae_video(self):
+		result = mie.get(self.link)
+		tree = html.fromstring(result.content)
+		linkdata = tree.xpath('//div[@id="view_container"]/div[@id="linkdatacontainer"]/div[@id="linkdata"]')[0]
+
+		self.link_id = tree.xpath('//input[@name="link_id"]/@value')[0]
+		self.rating = linkdata.xpath('h1/span[@id="ratevalue"]/text()')
+		if len(self.rating)!=0:
+			self.rating = self.rating[0].strip()
+		else:
+			self.rating = "<et ole kirjautunut>"
+		self.katsottu = linkdata.xpath('div[@id="linktoolstable"]//p/b/text()')[0]
+		self.user = linkdata.xpath('div[@id="linktoolstable"]//a/b/text()')
+		if len(self.user)!=0:
+			self.user = self.user[0]
+			i=2
+		else:
+			self.user = "anon"
+			i=1 #jos anon niin yksi rivi puuttuu alusta
+		self.date = linkdata.xpath('div[@id="linktoolstable"]//div[@id="linkinfo"]/p/text()')[i].strip()
+		self.date = re.search('\d+[.]\d+[.]\d+', self.date).group(0) #haetaan vain päivänmäärä
+
+		embedded = tree.xpath('//div[@id="viewbody_container"]/div[@id="viewbody"]/div[@id="viewembedded"]')[0]
+		youtube = embedded.xpath('iframe/@src')
+		flv = embedded.xpath('script/text()')
+
+		if len(flv) != 0:
+			self.url = re.search('https:.+[.]flv', flv[0]).group(0) #HUOM! jos logattuna niin https, muuten http
+		else:
+			ydl = YTelement(youtube[0])
+			self.url = ydl.video
+	
+	def hae_sessio(self, sessio):
+		tree = html.fromstring(sessio.content)
+		self.title = tree.xpath('//div[@id="view_container"]/div[@id="linkdatacontainer"]/div[@id="linkdata"]/h1/span[@id="linktitle"]/text()')
+		if len(self.title)!=0:
+			self.title = self.title[0]
+		else:
+			self.title = "<Ei nimikettä>"
+		self.link = sessio.url
+		self.hae_video()
+	
+	def hae_rating(self):
+		result = mie.hae(self.link)
+		tree = html.fromstring(result.content)
+		linkdata = tree.xpath('//div[@id="view_container"]/div[@id="linkdatacontainer"]/div[@id="linkdata"]')[0]
+		self.rating = linkdata.xpath('h1/span[@id="ratevalue"]/text()')[0].strip()
+		
+	def hae_kommentit(self):
+		result = mie.get(self.link)
+		tree = html.fromstring(result.content)
+		self.comments = list()
+		for kommentti in tree.xpath('//div[@id="list_comments"]/table/tr')[::2]:
+			self.comments.append(Kommentti(kommentti))
+		
+	def rate_video(self, rating):
+		payload={
+			'service_id':'1',
+			'target_id':self.link_id,
+			'points':rating
+			}
+		mie.post("https://naurunappula.com/rate.php", payload)
+		
+	def add_channel(self, kanava):
+		payload={
+			'link_id':self.link_id,
+			'group_id':kanava
+			}
+		mie.post("https://naurunappula.com/favadd.php", payload)
+
+
